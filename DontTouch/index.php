2a072d0ba247b47b5d2fcaad0e91987b9dec22fb
Tish61ka <49403327+Tish61ka@users.noreply.github.com>
2021-12-10 13:58:05 +0400

    Add files via upload

--- DontTouch/index.php
+++ DontTouch/index.php
@@ -1,3 +1,6 @@
+<?php
+    $i = 1;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,16 +16,83 @@
         <input id="a" name="text1" type="text" required placeholder="Введите содержание работы">
         <label for="b"></label>
         <input id="b" name="text2" type="text" required placeholder="Введите длительность">
-        <input type="submit" value="ОТпраВИТЬ">
+        <label class="button" for="c">Submit</label>
+        <input id="c" class="btn" type="submit" value="ОТпраВИТЬ">
     </form>
-    <?php 
-        $sql = "SELECT * FROM `zxc`";
-        while($row = $result->fetch_array()){
-            $qwe = $row["id"];
-            $qwe = $row["text1"];
-            $qwe = $row["text2"];
-        }
-    
+    <a href="./del.php"><button>Удалить всё</button></a>
+    <a href="#"><button class="btn-2">Построить график</button></a>
+    <a href="#"><button class="btn-3">Создать таблицу</button></a>
+    <div class="div">
+        <?php
+        require('connect.php'); 
+        $output = "SELECT * FROM `zxc`";
+        $result = mysqli_query($connect,$output);
+        while($row = mysqli_fetch_array($result)){
+            ?>  
+            <div class="p">
+            <?="<b>"."Содержание работ: " . "</b>" . $row["text1"];?><?='</br>'?>
+     
+            <?="<b>" ."Длительность работ: " . "</b>" .'<span id="giveNumber">' . $row["text2"] ."</span>";?>
+
+            </div>
+            <?php
+        }    
     ?>
+    <script>
+        let i = <?php $i?>
+        alert(i);
+    </script>
+    </div>
+    <table>
+        <tr>
+            <td>Событие</td>
+            <td>Тр</td>
+            <td>Тn</td>
+            <td>R = Tn - Tp</td>
+        </tr>   
+        <tr>
+            <td>0</td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+    <script src="script.js"></script>
 </body>
 </html>
---
+++ DontTouch/script.js
@@ -0,0 +1,5 @@
+let p = document.getElementById('giveNumber');
+let text = p.textContent;
+let number = Number(text);
+document.write(number);
+alert(number);
